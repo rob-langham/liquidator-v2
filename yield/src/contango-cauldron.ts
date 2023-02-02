acfdb2fd70feeef2cfe00245a79c695d7e5a6117
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2023-02-02 14:46:18 +0000

    Remove unnecessary stuff

--- yield/src/contango-cauldron.ts
+++ yield/src/contango-cauldron.ts
@@ -1,16 +1,7 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
-  AssetAdded as AssetAddedEvent,
-  DebtLimitsSet as DebtLimitsSetEvent,
-  IlkAdded as IlkAddedEvent,
-  RateOracleAdded as RateOracleAddedEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
-  SeriesAdded as SeriesAddedEvent,
-  SeriesMatured as SeriesMaturedEvent,
-  SpotOracleAdded as SpotOracleAddedEvent,
-  VaultBuilt as VaultBuiltEvent,
+  AssetAdded as AssetAddedEvent, IlkAdded as IlkAddedEvent, SeriesAdded as SeriesAddedEvent,
+  SeriesMatured as SeriesMaturedEvent, VaultBuilt as VaultBuiltEvent,
   VaultDestroyed as VaultDestroyedEvent,
   VaultGiven as VaultGivenEvent,
   VaultPoured as VaultPouredEvent,
@@ -18,20 +9,7 @@ import {
   VaultStirred as VaultStirredEvent,
   VaultTweaked as VaultTweakedEvent
 } from "../generated/ContangoCauldron/ContangoCauldron";
-import {
-  AssetEntity, DebtLimitsSet, InstrumentEntity,
-  RateOracleAdded,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  SeriesEntity,
-  SpotOracleAdded, VaultBuilt,
-  VaultDestroyed, VaultEntity, VaultGiven,
-  VaultPoured,
-  VaultRolled,
-  VaultStirred,
-  VaultTweaked
-} from "../generated/schema";
+import { AssetEntity, InstrumentEntity, SeriesEntity, VaultEntity } from "../generated/schema";
 
 export function handleAssetAdded(event: AssetAddedEvent): void {
   const asset = new AssetEntity(event.params.assetId.toHex());
@@ -40,92 +18,12 @@ export function handleAssetAdded(event: AssetAddedEvent): void {
   asset.save();
 }
 
-export function handleDebtLimitsSet(event: DebtLimitsSetEvent): void {
-  let entity = new DebtLimitsSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.baseId = event.params.baseId;
-  entity.ilkId = event.params.ilkId;
-  entity.max = event.params.max;
-  entity.min = event.params.min;
-  entity.dec = event.params.dec;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-}
-
 export function handleIlkAdded(event: IlkAddedEvent): void {
   const instrument = new InstrumentEntity(event.params.seriesId.concat(event.params.ilkId));
   instrument.seriesId = event.params.seriesId;
   instrument.ilkId = event.params.ilkId;
   instrument.series = event.params.seriesId;
   instrument.save();
-}
-
-export function handleRateOracleAdded(event: RateOracleAddedEvent): void {
-  let entity = new RateOracleAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.baseId = event.params.baseId;
-  entity.oracle = event.params.oracle;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.role = event.params.role;
-  entity.newAdminRole = event.params.newAdminRole;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.role = event.params.role;
-  entity.account = event.params.account;
-  entity.sender = event.params.sender;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.role = event.params.role;
-  entity.account = event.params.account;
-  entity.sender = event.params.sender;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
 }
 
 export function handleSeriesAdded(event: SeriesAddedEvent): void {
@@ -147,39 +45,7 @@ export function handleSeriesMatured(event: SeriesMaturedEvent): void {
   }
 }
 
-export function handleSpotOracleAdded(event: SpotOracleAddedEvent): void {
-  let entity = new SpotOracleAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.baseId = event.params.baseId;
-  entity.ilkId = event.params.ilkId;
-  entity.oracle = event.params.oracle;
-  entity.ratio = event.params.ratio;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-}
-
 export function handleVaultBuilt(event: VaultBuiltEvent): void {
-  let entity = new VaultBuilt(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-  entity.owner = event.params.owner;
-  entity.seriesId = event.params.seriesId;
-  entity.ilkId = event.params.ilkId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.vaultId,
     event.params.seriesId,
@@ -192,38 +58,12 @@ export function handleVaultBuilt(event: VaultBuiltEvent): void {
 }
 
 export function handleVaultDestroyed(event: VaultDestroyedEvent): void {
-  let entity = new VaultDestroyed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(event.params.vaultId, null, null, null, null, null, true);
-
   // store.remove('Vault', "0x" + BigInt.fromUnsignedBytes(event.params.vaultId).toHex().substring(2).padStart(24, '0'))
   // store.remove("Vault", event.params.vaultId.toHex());
 }
 
 export function handleVaultGiven(event: VaultGivenEvent): void {
-  let entity = new VaultGiven(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-  entity.receiver = event.params.receiver;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.vaultId,
     null,
@@ -236,22 +76,6 @@ export function handleVaultGiven(event: VaultGivenEvent): void {
 }
 
 export function handleVaultPoured(event: VaultPouredEvent): void {
-  let entity = new VaultPoured(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-  entity.seriesId = event.params.seriesId;
-  entity.ilkId = event.params.ilkId;
-  entity.ink = event.params.ink;
-  entity.art = event.params.art;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.vaultId,
     event.params.seriesId,
@@ -264,20 +88,6 @@ export function handleVaultPoured(event: VaultPouredEvent): void {
 }
 
 export function handleVaultRolled(event: VaultRolledEvent): void {
-  let entity = new VaultRolled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-  entity.seriesId = event.params.seriesId;
-  entity.art = event.params.art;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.vaultId,
     event.params.seriesId,
@@ -290,21 +100,6 @@ export function handleVaultRolled(event: VaultRolledEvent): void {
 }
 
 export function handleVaultStirred(event: VaultStirredEvent): void {
-  let entity = new VaultStirred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.from = event.params.from;
-  entity.to = event.params.to;
-  entity.ink = event.params.ink;
-  entity.art = event.params.art;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.from,
     null,
@@ -326,20 +121,6 @@ export function handleVaultStirred(event: VaultStirredEvent): void {
 }
 
 export function handleVaultTweaked(event: VaultTweakedEvent): void {
-  let entity = new VaultTweaked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.vaultId = event.params.vaultId;
-  entity.seriesId = event.params.seriesId;
-  entity.ilkId = event.params.ilkId;
-
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
-  entity.index = event.block.number.leftShift(32).plus(event.logIndex);
-
-  entity.save();
-
   storeVaultUpdate(
     event.params.vaultId,
     event.params.seriesId,
